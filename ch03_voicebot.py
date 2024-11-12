b295012421918a7aadff335722e5ce8a83d13d8c
aquapsy <tnsdud987@gmail.com>
2024-11-12 16:23:16 +0900

    Add files via upload

--- ch03_voicebot.py
+++ ch03_voicebot.py
@@ -84,7 +84,7 @@ def main():
     # 사이드바 생성
     with st.sidebar:
         # Open AI API 키 입력받기
-        OPENAI_API_KEY = "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"
+        OPENAI_API_KEY = "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"
         api_key = st.text_input(label="OPENAI API 키", placeholder="Enter Your API Key", value=OPENAI_API_KEY, type="password")
 
         if api_key:
